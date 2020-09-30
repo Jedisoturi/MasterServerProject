5ea26ac9259e74e7f300f3d654357bee23d90836
HenriLagerroos <43806027+HenriLagerroos@users.noreply.github.com>
2020-09-30 19:10:52 +0300

    Player suicide

--- MasterServer/Objects/Player/Player.cs
+++ MasterServer/Objects/Player/Player.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MasterServer.Objects.Player
+public class Player
 {
-    public class Player
+    public Player()
     {
+        achievements = new bool[Enum.GetNames(typeof(Achievement)).Length];
     }
+
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public int Score { get; set; }
+    public int Level { get; set; }
+    public DateTime CreationTime { get; set; }
+    public bool[] achievements;
 }
