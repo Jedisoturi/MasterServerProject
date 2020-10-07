e544bcdd5f4fee53861e3d6a23a9e12275bb474b
Joona Eskonpekka <ejaa7991@gmail.com>
2020-10-07 09:28:23 +0300

    pikku muutos

--- MasterServer/ActionFilters/AppAuthenticationFilter.cs
+++ MasterServer/ActionFilters/AppAuthenticationFilter.cs
@@ -24,12 +24,8 @@ namespace MasterServer
             if (!request.Headers.TryGetValue("TimeStamp", out StringValues timeStampValue)) throw new AppAuthenticationException($"No timeStamp provided");
             string timeStampString = timeStampValue.FirstOrDefault();
 
-
-
             // Check if timeout
             if (!DateTime.TryParse(timeStampString, out DateTime timeStamp)) throw new AppAuthenticationException($"Invalid TimeStamp");
-            Console.WriteLine(timeStampString);
-            Console.WriteLine(DateTime.UtcNow.ToString());
             if (timeStamp.AddMinutes(1) < DateTime.UtcNow) throw new Exception($"Request is too old");
 
             // Get signature
@@ -41,7 +37,7 @@ namespace MasterServer
 
             // Read body as json
             var body = new StreamReader(request.Body);
-            //The modelbinder has already read the stream and need to reset the stream index
+            // The modelbinder has already read the stream and need to reset the stream index
             body.BaseStream.Seek(0, SeekOrigin.Begin);
             var bodyString = (await body.ReadToEndAsync()).ToLower();
 
